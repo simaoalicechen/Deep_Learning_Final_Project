3060cf2bb95a703c0acc211a4230cbbf2978ea87
simaoalicechen <alicechen005@gmail.com>
2024-04-18 00:03:05 -0400

    changed the sample_interval so that more pictures can be generated. Quality not good, need further fine tuning

--- gan.py
+++ gan.py
@@ -18,7 +18,7 @@ from torchinfo import summary
 os.makedirs("images", exist_ok=True)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=20, help="number of epochs of training")
+parser.add_argument("--n_epochs", type=int, default=40, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0001, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
@@ -27,7 +27,7 @@ parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads 
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
 parser.add_argument("--img_size", type=int, default=224, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=3, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
+parser.add_argument("--sample_interval", type=int, default=10, help="interval betwen image samples")
 opt = parser.parse_args()
 print(opt)
 
@@ -120,7 +120,7 @@ Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 # ----------
 #  Training
 # ----------
-
+n = 0
 for epoch in range(opt.n_epochs):
     for i, (imgs, _) in enumerate(trainloader):
 
@@ -169,5 +169,10 @@ for epoch in range(opt.n_epochs):
         )
 
         batches_done = epoch * len(trainloader) + i
+        print(len(trainloader))
+        print(batches_done)
+        print(opt.sample_interval)
         if batches_done % opt.sample_interval == 0:
+            print(f"pictures {n}")
+            n += 1
             save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
