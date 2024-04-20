a01d134f80b3deddc68ff6a9e214affdd60ff9a7
simaoalicechen <alicechen005@gmail.com>
2024-04-20 01:28:10 -0400

    add dragan, dcgan(in progress), and wassersteiGan(out of memory), and less-than-ideal images from dragan and original gan that motivate to move to pre-trained models and for later report uses

---
+++ wassersteinGan.py
@@ -0,0 +1,198 @@
+import argparse
+import os
+import numpy as np
+import math
+import sys
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.autograd as autograd
+import torch
+
+os.makedirs("images", exist_ok=True)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_epochs", type=int, default=50000, help="number of epochs of training")
+parser.add_argument("--batch_size", type=int, default=8, help="size of the batches")
+parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+parser.add_argument("--img_size", type=int, default=244, help="size of each image dimension")
+parser.add_argument("--channels", type=int, default=3, help="number of image channels")
+parser.add_argument("--n_critic", type=int, default=5, help="number of training steps for discriminator per iter")
+parser.add_argument("--clip_value", type=float, default=0.01, help="lower and upper clip value for disc. weights")
+parser.add_argument("--sample_interval", type=int, default=1000, help="interval betwen image samples")
+opt = parser.parse_args()
+print(opt)
+
+img_shape = (opt.channels, opt.img_size, opt.img_size)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *block(opt.latent_dim, 128, normalize=False),
+            *block(128, 64),
+            *block(64, 64),
+            *block(64, 256),
+            nn.Linear(256, int(np.prod(img_shape))),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        img = self.model(z)
+        img = img.view(img.shape[0], *img_shape)
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+            print(int(np.prod(img_shape))), 
+            nn.Linear(int(np.prod(img_shape)), 150528),  # Adjusted output size
+            nn.LeakyReLU(0.2, inplace=True),
+            # nn.Linear(150528, 256),  # Adjusted input size
+            # nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(150528, 1),
+        )
+
+    def forward(self, img):
+        img_flat = img.view(img.shape[0], -1)
+        validity = self.model(img_flat)
+        return validity
+
+
+k = 2
+p = 6
+
+
+# Initialize generator and discriminator
+generator = Generator()
+discriminator = Discriminator()
+
+# if device:
+generator.to(device)
+discriminator.to(device)
+
+# Define the transformation to be applied to the images
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+# Configure data loader
+# Create a custom dataset from the ImageFolder class
+train_dataset = ImageFolder(root="./data/ImageNet/train.X4", transform=transform)
+val_dataset = ImageFolder(root="./data/ImageNet/val.X", transform=transform)
+
+# Create a DataLoader to facilitate batch processing
+batch_size = 8 
+trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+valloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+
+
+# Optimizers
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+# ----------
+#  Training
+# ----------
+
+
+batches_done = 0
+for epoch in range(opt.n_epochs):
+    for i, (imgs, _) in enumerate(trainloader):
+
+        # Configure input
+        real_imgs = Variable(imgs.type(Tensor), requires_grad=True)
+
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+
+        optimizer_D.zero_grad()
+
+        # Sample noise as generator input
+        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
+
+        # Generate a batch of images
+        fake_imgs = generator(z)
+
+        # Real images
+        real_validity = discriminator(real_imgs)
+        # Fake images
+        fake_validity = discriminator(fake_imgs)
+
+        # Compute W-div gradient penalty
+        real_grad_out = Variable(Tensor(real_imgs.size(0), 1).fill_(1.0), requires_grad=False)
+        real_grad = autograd.grad(
+            real_validity, real_imgs, real_grad_out, create_graph=True, retain_graph=True, only_inputs=True
+        )[0]
+        real_grad_norm = real_grad.view(real_grad.size(0), -1).pow(2).sum(1) ** (p / 2)
+
+        fake_grad_out = Variable(Tensor(fake_imgs.size(0), 1).fill_(1.0), requires_grad=False)
+        fake_grad = autograd.grad(
+            fake_validity, fake_imgs, fake_grad_out, create_graph=True, retain_graph=True, only_inputs=True
+        )[0]
+        fake_grad_norm = fake_grad.view(fake_grad.size(0), -1).pow(2).sum(1) ** (p / 2)
+
+        div_gp = torch.mean(real_grad_norm + fake_grad_norm) * k / 2
+
+        # Adversarial loss
+        d_loss = -torch.mean(real_validity) + torch.mean(fake_validity) + div_gp
+
+        d_loss.backward()
+        optimizer_D.step()
+
+        optimizer_G.zero_grad()
+
+        # Train the generator every n_critic steps
+        if i % opt.n_critic == 0:
+
+            # -----------------
+            #  Train Generator
+            # -----------------
+
+            # Generate a batch of images
+            fake_imgs = generator(z)
+            # Loss measures generator's ability to fool the discriminator
+            # Train on fake images
+            fake_validity = discriminator(fake_imgs)
+            g_loss = -torch.mean(fake_validity)
+
+            g_loss.backward()
+            optimizer_G.step()
+
+            print(
+                "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+                % (epoch, opt.n_epochs, i, len(trainloader), d_loss.item(), g_loss.item())
+            )
+
+            if batches_done % opt.sample_interval == 0:
+                save_image(fake_imgs.data[:25], f"images/%d_{epoch}.png" % batches_done, nrow=5, normalize=True)
+
+            batches_done += opt.n_critic
